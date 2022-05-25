543692b802de3566e052ca48609b1fd9bdef3719
Ilya Leoshkevich <iii@linux.ibm.com>
2022-05-25 20:36:10 +0200

    gdb.base/: Introduce jit-protocol-util.h
    jit-elf-fork-main, jit-elf-main and jit-reader-host use the same
    boilerplate to register and unregister JITed code.  Move it to a new
    header that can be shared with performance tests.

---
+++ gdb/testsuite/gdb.base/jit-protocol-util.h
@@ -0,0 +1,74 @@
+/* This test program is part of GDB, the GNU debugger.
+
+   Copyright (C) 2022 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Frequently used functions for testing JITed objects.  */
+
+#ifndef JIT_PROTOCOL_UTIL_H
+#define JIT_PROTOCOL_UTIL_H
+
+#include "jit-protocol.h"
+#include <stddef.h>
+
+/* Return whether there are no registered JITed objects.  */
+static int
+jit_empty (void)
+{
+  return __jit_debug_descriptor.relevant_entry == NULL;
+}
+
+/* Register JITed object.  symfile_addr and symfile_size must be set.  */
+static void
+jit_push_back (struct jit_code_entry *entry)
+{
+  entry->prev_entry = __jit_debug_descriptor.relevant_entry;
+  __jit_debug_descriptor.relevant_entry = entry;
+
+  if (entry->prev_entry != NULL)
+    entry->prev_entry->next_entry = entry;
+  else
+    __jit_debug_descriptor.first_entry = entry;
+
+  /* Notify GDB.  */
+  __jit_debug_descriptor.action_flag = JIT_REGISTER;
+  __jit_debug_register_code ();
+}
+
+/* Unregister the last registered JITed object.  */
+static struct jit_code_entry *
+jit_pop_back (void)
+{
+  struct jit_code_entry *const entry = __jit_debug_descriptor.relevant_entry;
+  struct jit_code_entry *const prev_entry = entry->prev_entry;
+
+  if (prev_entry != NULL)
+    {
+      prev_entry->next_entry = NULL;
+      entry->prev_entry = NULL;
+    }
+  else
+    __jit_debug_descriptor.first_entry = NULL;
+
+  /* Notify GDB.  */
+  __jit_debug_descriptor.action_flag = JIT_UNREGISTER;
+  __jit_debug_register_code ();
+
+  __jit_debug_descriptor.relevant_entry = prev_entry;
+
+  return entry;
+}
+
+#endif /* JIT_PROTOCOL_UTIL_H */
